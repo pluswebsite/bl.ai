2041a99b232bf52d9e8e9c21f9dd41a7b0a4fcbe
pluswebsite <98026305+pluswebsite@users.noreply.github.com>
2023-03-03 20:45:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function ai(q, server){
+function openai(q){
 const options = {
   method: 'POST',
   headers: {
@@ -12,7 +12,32 @@ const options = {
     show_original_response: false,
     temperature: 0,
     max_tokens: 250,
-    providers: server,
+    providers: "openai",
+    text: q
+  })
+};
+
+fetch('https://api.edenai.run/v2/text/generation', options)
+  .then(response => response.json())
+  .then(response => console.log(response.openai.generated_text))
+  .catch(err => console.error(err));
+}
+
+function cohere(q){
+const options = {
+  method: 'POST',
+  headers: {
+    accept: 'application/json',
+    'content-type': 'application/json',
+    authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiODZlZjQ3ZmMtYTIyNi00ZDg2LWFhNWItNTBhMDI2Mzg3MWEwIiwidHlwZSI6ImFwaV90b2tlbiJ9.tAaEu-8ps0n0N91NAJJAtsHTTqRU2hDJP3OtvNpoG8c'
+  },
+  body: JSON.stringify({
+    response_as_dict: true,
+    attributes_as_list: false,
+    show_original_response: false,
+    temperature: 0,
+    max_tokens: 250,
+    providers: "cohere",
     text: q
   })
 };
